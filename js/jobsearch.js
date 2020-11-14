b41cc5673264ea0f5a98ee9234d773ff022654e8
Lukasz <beowfull@gmail.com>
2020-11-14 18:01:37 +0100

    added insert new job page. Fixed accessibility issues with homepage and search job page. Added example job to preview

--- js/jobsearch.js
+++ js/jobsearch.js
@@ -60,11 +60,10 @@ function showJobDetails(ev) {
             openModal.classList.add("is-clipped");
         } else {
             const jobListingsContainer = document.querySelector(
-                ".work-preview--empty"
+                ".work-preview"
             );
-            const objectResult = JSON.parse(jobsList);
             jobListingsContainer.innerHTML = "";
-            jobListingsContainer.append(JSON.stringify(objectResult));
+            jobListingsContainer.innerHTML = jobsList;
         }
     }
 }
@@ -97,14 +96,101 @@ function getSearchInputReferences() {
     const locationName = document.querySelector("[name=location]");
     return { jobName, locationName };
 }
-const jobsList = '{"result":true, "count":42}';
-const jobssList = {
-    title: "Programmatore Java",
-    location: "Pisa",
-    region: "Toscana",
-    description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus labore non ipsam sequi vitae, iste, at nobis modi voluptatum porro officia accusantium totam quasi laborum! Molestias provident quidem molestiae impedit! Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus labore non ipsam sequi vitae, iste, at nobis modi voluptatum porro officia accusantium totam quasi laborum! Molestias provident quidem molestiae impedit!",
-    data: "12/11/2020",
-    contract: "Tempo Indeterminato",
-    verified: "true",
-};
+const jobsList = `
+<article class="job-details">
+<header class="job-header">
+    <h3 class="job-details__title">System Administrator</h3>
+</header>
+<main class="job-detais-info">
+    <article class="info__main">
+        <p class="info__location">Pisa, Toscana</p>
+        <div class="job-company-info">
+            <img
+                class="job-company-info__logo"
+                src="images/logoProntoLavoro.svg"
+                alt="company logo"
+                width="50px"
+                height="50px"
+            />
+            <p class="job-listing__company">Hyperborea</p>
+        </div>
+    </article>
+    <article class="info__description">
+        <p>
+            La figura che cerchiamo avrà il compito di
+            predisporre e manutenere l’infrastruttura fisica
+            e virtuale di Hyperborea allo scopo di
+            permettere agli sviluppatori e gli altri
+            dipendenti della nostra azienda di svolgere il
+            proprio lavoro. In aggiunta dovrà supportare i
+            team di sviluppo nel dispiegamento e
+            manutenzione dei prodotti Arianna e docLife in
+            modalità “on premise”.
+        </p>
+        <h3>Responsabilità del Sistemista</h3>
+        <ol>
+            <li>
+                si occupa di mantenere l’infrastruttura
+                locale: routing, firewall, cluster di server
+                fisici, connettività verso l’esterno;
+            </li>
+            <li>
+                amministra il cluster di server mediante
+                sistema di virtualizzazione VMWare;
+            </li>
+            <li>
+                amministra i server virtuali Linux di
+                sviluppo e produzione;
+            </li>
+            <li>
+                amministra i servizi e gli account in
+                utilizzo (suite Microsoft, suite Google,
+                suite Jira…);
+            </li>
+            <li>
+                predispone l’hardware installando il sistema
+                operativo e tutti i componenti software
+                necessari all’operatività degli utenti;
+            </li>
+            <li>
+                mantiene la documentazione
+                dell’infrastruttura locale e della
+                risoluzione dei problemi;
+            </li>
+            <li>
+                supporta gli utenti in caso di problemi di
+                natura hardware o software
+            </li>
+        </ol>
+        <h3>Competenze Richieste (Requirements)</h3>
+        <ol>
+            <li>
+                laurea di primo livello in informatica o
+                ingegneria informatica, con una buona
+                capacità di relazione con i colleghi ed i
+                clienti ed in grado di lavorare
+                autonomamente. In alternativa è accettabile
+                l’assenza di titolo a fronte di
+                un’esperienza nel ruolo di Sistemista non
+                inferiore ai 3 anni:
+            </li>
+            <li>
+                ottime capacità di amministrazione su
+                sistemi operativi server Windows e Linux;
+            </li>
+            <li>
+                ottima conoscenza di aspetti prettamente
+                sistemistici: reti di calcolatori, firewall,
+                VPN, web server, certificati e protocolli
+                sicuri, etc.
+            </li>
+            <li>
+                buona conoscenza di linguaggi di scripting
+                come Ruby, Python, Bash;
+            </li>
+            <li>conoscenza della lingua inglese.</li>
+        </ol>
+        <button class="btn-apply button is-outlined">Candidati</button>
+    </article>
+</main>
+</article>`;
